b367d4aaeb52cda28a39e1cbbcccb0ef3ba7f0ad
Justin Fagnani <justinfagnani@google.com>
2019-05-10 22:16:36 -0700

    Add a formatString directive

--- lit-html/render/formatted.js
+++ lit-html/render/formatted.js
@@ -2,26 +2,58 @@ import * as bench from '/bench.js';
 
 import {longStrings} from '../../common/render/strings.js';
 import {unsafeHTML} from '../node_modules/lit-html/directives/unsafe-html.js';
-import {html, render} from '../node_modules/lit-html/lit-html.js';
+import {html, render, directive} from '../node_modules/lit-html/lit-html.js';
 
 function getStyle(run) {
-  let classes = new Array();
+  let className = '';
   if (run.bold) {
-    classes.push('bold');
+    className += ' bold';
   }
   if (run.deemphasize) {
-    classes.push('deemphasize');
+    className += ' deemphasize';
   }
   if (run.italics) {
-    classes.push('italics');
+    className += ' italics';
   }
   if (run.strikethrough) {
-    classes.push('strikethrough');
+    className += ' strikethrough';
   }
-  return classes.join(' ');
+  return className;
 }
 
+const previousValues = new WeakMap();
+
+export const formatString = directive((str) => (part) => {
+
+  const previousValue = previousValues.get(part);
+  // assumes formatted strings objects are immutable
+  if (previousValue !== undefined && str === previousValue) {
+    return;
+  }
+
+  const span = document.createElement('span');
+  if (str.simpleText !== undefined) {
+    span.innerText = str.simpleText;
+  } else {
+    let node;
+    for (const run of str.runs) {
+      if (run.navigationEndpoint !== undefined) {
+        node = document.createElement('a');
+        node.href = '#';
+      } else {
+        node = document.createElement('span');
+        node.className = getStyle(run);
+      }
+      node.innerText = run.text;
+      span.appendChild(node);
+    }
+  }
+  part.setValue(span);
+  previousValues.set(part, str);
+});
+
 const unsafe = new URL(window.location.href).searchParams.has('unsafe');
+const format = new URL(window.location.href).searchParams.has('format');
 
 let template;
 if (unsafe) {
@@ -34,10 +66,12 @@ if (unsafe) {
                                      .map(
                                          (run, i) => run.navigationEndpoint ?
                                              `<a href="#">${run.text}</a>` :
-                                             `<span class=${getStyle(run)}>${
+                                             `<span class="${getStyle(run)}">${
                                                  run.text}</span>`)
                                      .join(''))}</span>`);
   };
+} else if (format) {
+  template = (instance) => instance.data.map((str, i) => formatString(str));
 } else {
   template = function template(instance) {
     return instance.data.map(
